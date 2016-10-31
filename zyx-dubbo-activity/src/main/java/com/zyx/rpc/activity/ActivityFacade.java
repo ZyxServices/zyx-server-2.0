88710d4c44116048a3f4b9e3f7010c40e3e68b72
MrDeng <396187665@qq.com>
2016-10-31 18:23:55 +0800

    【merge】

---
+++ zyx-dubbo-activity/src/main/java/com/zyx/rpc/activity/ActivityFacade.java
@@ -0,0 +1,19 @@
+package com.zyx.rpc.activity;
+
+import java.util.Map;
+
+/**
+ * Created by Rainbow on 16-6-12.
+ *
+ * @author SubDong
+ * @version V1.0
+ *          Copyright (c)2016 tyj-版权所有
+ * @title com.rpc.activity
+ */
+public interface ActivityFacade {
+
+
+    Map<String, Object> insertActivity();
+
+
+}
