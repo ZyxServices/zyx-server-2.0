6b0b877de6e1701ca4251c379b9c277415d418cc
RAINBOW-SUBDONG\Rainbow <shuzichao@126.com>
2016-11-17 17:12:52 +0800

    [add] 首推

---
+++ zyx-vo/src/main/java/com/zyx/vo/system/SearchDevaVo.java
@@ -0,0 +1,12 @@
+package com.zyx.vo.system;
+
+/**
+ * Created by Rainbow
+ *
+ * @author SubDong
+ * @version V2.0
+ *          Copyright (c)2016 zyx-版权所有
+ * @since 2016/11/17
+ */
+public class SearchDevaVo {
+}
